2690db3b6aed40e684e05ed9aa9ddf5ed903dcbd
Kyle Binger <kyle.binger3@ontario.ca>
2025-07-25 13:19:35 -0400

    chore: commit version 1.7.0

--- src/scripts/ontario-search.js
+++ src/scripts/ontario-search.js
@@ -7,3 +7,12 @@ function doSearch() {
 function handleSearch() {
 	console.log('Searching...');
 }
+
+function addSearchResetOnClickListener(searchResetButtonId, searchInputId) {
+	document.getElementById(searchResetButtonId).addEventListener('click', () => {
+		document.getElementById(searchInputId).focus();
+	});
+}
+
+addSearchResetOnClickListener('search-example-1-reset-button', 'search-example-1');
+addSearchResetOnClickListener('search-example-2-reset-button', 'search-example-2');
